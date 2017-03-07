d2301ae66e9ec4c1f0299a9b54b8ea208402162f
uNickHow <NickLin@UserdeMacBook-Pro.local>
2017-03-07 12:18:32 +0800

    finished CRUD by watching video

--- photo_album/app/controllers/photos_controller.rb
+++ photo_album/app/controllers/photos_controller.rb
@@ -5,4 +5,44 @@ class PhotosController < ApplicationController
 
 	end
 
+	def new 
+		@photo = Photo.new
+
+	end
+
+	def create
+	 @photo = Photo.new(photo_params)
+	 @photo.save
+
+	 redirect_to photos_url
+	end
+
+	def show
+	 @photo = Photo.find(params[:id])
+	end
+
+	def edit
+	 @photo = Photo.find(params[:id])
+	end
+
+	def update
+	 @photo = Photo.find(params[:id])
+	 @photo.update(photo_params)
+
+	 redirect_to photo_url
+	end
+
+	def destroy
+	 @photo = Photo.find(params[:id])
+	 @photo.destroy
+
+	 redirect_to photos_url
+	end
+
+	private
+
+	def photo_params
+	 params.require(:photo).permit(:title, :date, :description, :file_location)
+	end
+
 end
